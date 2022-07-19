df296da51c20c5d318c23fa28d5dcbb2a58ba792
yania-pena <yania.pena@udla.edu.ec>
2022-07-19 00:40:33 -0500

    Update app.js

--- app.js
+++ app.js
@@ -41,8 +41,12 @@ app.use('/tipos',tiposRouter);
  // next(createError(404));
 //});
 // error handler
-const PORT = process.env.PORT || 4000 
-app.listen(PORT, function() {
-  console.log("Servidor escuchando en el puerto",PORT)
- });
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 module.exports = app;
